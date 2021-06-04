a49bfabcdeb29cbf5e767a9054dde80c78ccb25d
Miome <miome.geo@yahoo.com>
2021-06-04 13:11:31 -0400

    First commit

---
+++ OneDayAtATime/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace OneDayAtATime
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.Saved += this.onSaved;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        private void onSaved(object sender, SavedEventArgs e)
+        {
+            System.Environment.Exit(0);
+
+        }
+    }
+}
